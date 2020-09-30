8f35ecd79c54b76d70a8b24c00ef8afef16b51d3
Dan Hermosillo <danielahermosillohdz@gmail.com>
2020-09-30 14:07:40 -0500

    commiting the website

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function(){
+  var burger = document.querySelector('.burger-container'),
+      header = document.querySelector('.header'),
+      menu = document.querySelector('.menu');
+  
+  burger.onclick = function() {
+      header.classList.toggle('menu-opened');
+  }
+  
+  menu.onclick = function() {
+      header.classList.toggle('menu-opened');
+  }
+  
+}());
