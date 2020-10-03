d051c21c43073f593b211c61d8e79a67dc369ae7
Dan Hermosillo <danielahermosillohdz@gmail.com>
2020-10-03 13:40:58 -0500

    improves, lines, contactform php

---
+++ contactform.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+if (isset($_POST['submit'])) {
+  $nombre = $_POST['Nombre'];
+  $apellido = $_POST['Apellido'];
+  $correoDe = $_POST['Correo'];
+  $empresa = $_POST['Empresa'];
+  $mensaje = $_POST['Mensaje'];
+
+  $correoA = "danielahermosillohdz@gmail.com";
+  $headers = "Mensaje de: ".$correoDe;
+  $txt = "Has recibido un mensaje de ".$nombre.".\n\n"$mensaje;
+
+  mail($correoA, $txt, $headers);
+  header("Location: index.php?mailsend")
+}
+
